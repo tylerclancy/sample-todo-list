0d9cc6f6890ea35106b85b1c1456019a6bfeae56
Tyler <tylerclancy030@gmail.com>
2021-12-09 12:59:39 -0600

    Add React boilerplate, create component with initial state

---
+++ index.js
@@ -0,0 +1,29 @@
+function App() {
+  const [todos, setTodos] = React.useState([
+    {
+      text: 'learn react',
+      isCompleted: false,
+    },
+    {
+      text: 'drink unfathomable amounts of Mountain Dew',
+      isCompleted: false,
+    },
+    {
+      text: 'build todo app',
+      isCompleted: false,
+    },
+    {
+      text: 'get offline raided',
+      isCompleted: false,
+    },
+  ]);
+  return (
+    <>
+      {todos.map((todo, i) => (
+        <div key={i}>{todo.text}</div>
+      ))}
+    </>
+  );
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
